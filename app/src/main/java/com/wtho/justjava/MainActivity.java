21cf3c2b2e1795b9d41bcbd7a7c7b9d476ed40d5
Winnie <wtho.waithet@gmail.com>
2020-02-25 15:08:25 +0630

    Add the order
    Add new views , add the order and calcuate the price.

--- app/src/main/java/com/wtho/justjava/MainActivity.java
+++ app/src/main/java/com/wtho/justjava/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+   int quantity = 0;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +21,18 @@ public class MainActivity extends AppCompatActivity {
     * This method is called when the order button is clicked.
     */
    public void submitOrder(View view) {
-      int numberOfCoffees = 2;
-      display(numberOfCoffees);
-      displayPrice(numberOfCoffees * 5);
+      display(quantity);
+      displayPrice(quantity * 500);
+   }
+
+   public void increment(View view) {
+      quantity = quantity + 1;
+      display(quantity);
+   }
+
+   public void decrement(View view) {
+      quantity = quantity - 1;
+      display(quantity);
    }
 
    /**
