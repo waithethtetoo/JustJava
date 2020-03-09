9f5e0c9839e7e7ad98ff201845cd9681ba5c12e6
Winnie <wtho.waithet@gmail.com>
2020-03-09 16:55:41 +0630

    Change UI and set up Localization
    - add scrollView, editText,
    - change UI and
    - add Spanish Language

--- app/src/main/java/com/wtho/justjava/MainActivity.java
+++ app/src/main/java/com/wtho/justjava/MainActivity.java
@@ -2,38 +2,68 @@ package com.wtho.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-   int quantity = 0;
-   int price = 500;
-   TextView orderSummaryTextView;
+   private int quantity = 2;
+   private int price = 5;
+   private int totalPrice = 0;
+   private EditText edtUserName;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+      edtUserName = (EditText) findViewById(R.id.edt_user_name);
    }
 
    /**
     * This method is called when the order button is clicked.
     */
    public void submitOrder(View view) {
-      int totalPrice = calculatePrice(quantity, price);
-      createOrderSummary(totalPrice);
+      CheckBox chkWhippedCream = (CheckBox) findViewById(R.id.checkbox_whipped_cream);
+      CheckBox chkChocolate = (CheckBox) findViewById(R.id.checkbox_chocolate);
+      boolean whippedCreamChecked = chkWhippedCream.isChecked();
+      boolean chocolateCheck = chkChocolate.isChecked();
+      String name = edtUserName.getText().toString();
+      if (whippedCreamChecked == true) {
+         totalPrice = calculatePrice(quantity, price + 1);
+      } else if (chocolateCheck == true) {
+         totalPrice = calculatePrice(quantity, price + 2);
+      } else {
+         totalPrice = calculatePrice(quantity, price);
+      }
+      createOrderSummary(name, totalPrice, whippedCreamChecked, chocolateCheck);
+
+      Intent intent = new Intent(Intent.ACTION_SENDTO);
+      intent.setData(Uri.parse("mailto:"));
+      intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + name);
+      if (intent.resolveActivity(getPackageManager()) != null) {
+         startActivity(intent);
+      }
    }
 
    public void increment(View view) {
+      if (quantity == 100) {
+         Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+      }
       quantity = quantity + 1;
       displayQuantity(quantity);
    }
 
    public void decrement(View view) {
+      if (quantity == 1) {
+         Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+      }
       quantity = quantity - 1;
       displayQuantity(quantity);
    }
@@ -42,16 +72,16 @@ public class MainActivity extends AppCompatActivity {
     * Calculate Price
     **/
    private int calculatePrice(int cupCount, int price) {
-      int totalPrice = cupCount * price;
+      totalPrice = cupCount * price;
       return totalPrice;
    }
 
    /**
     * create order summary
     **/
-   private void createOrderSummary(int priceOfOrder) {
-      String message = "Name: Winnie" + "\n" + "Quantity: " + quantity + "\n" + "Total: $" + priceOfOrder + "\n" + "Thank You!";
-      displayMessage(message);
+   private void createOrderSummary(String name, int priceOfOrder, boolean creamChecked, boolean chocolateChecked) {
+      String message = "Name: " + name + "\n" + "Add Whipped cream ?" + creamChecked + "\n" + "Add Chocolate ? " + chocolateChecked + "\n" +
+              "Quantity: " + quantity + "\n" + "Total: $ " + priceOfOrder + "\n" + "Thank You!";
    }
 
    /**
@@ -62,10 +92,4 @@ public class MainActivity extends AppCompatActivity {
       quantityTextView.setText("" + number);
    }
 
-   /**
-    * This method displays the given text on the screen.
-    */
-   private void displayMessage(String message) {
-      orderSummaryTextView.setText(message);
-   }
 }
